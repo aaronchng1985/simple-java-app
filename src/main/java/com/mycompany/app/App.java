1e5f8ed24121d78aa8b6a20a5a1e0aad78d7f324
aaronchng1985 <aaron.851111@gmail.com>
2026-04-07 14:39:53 +0800

    changed!

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Give me MONEY, do not hesitate, if not you'll die, no body going to save you, hahah!");
+        System.out.println("Give me MONEY, do not hesitate, if not you'll die, no body going to save you, hahahah!");
     }
 
     public String getMessage() {
